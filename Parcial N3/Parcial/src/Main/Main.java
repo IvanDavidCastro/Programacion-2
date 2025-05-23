d87e59643a8a265c685064b918a41b9ac79a31a3
IvanDavidCastro <ivandavid.castrojimenez@unitecnar.edu.co>
2025-05-23 15:19:35 -0500

    Add files via upload

---
+++ Parcial N3/Parcial/src/Main/Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+import FORMULARIO.JFmMatriz;
+/**
+ *
+ * @author ivan
+ */
+public class Main {
+    public static void main(String[] args) {      
+        new JFmMatriz().setVisible(true);
+    }
+}
